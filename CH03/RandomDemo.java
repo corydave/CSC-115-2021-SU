a7fd8e0bb7aefae25c3505dbce16ed013290d889
corydave <corydave@users.noreply.github.com>
2021-06-22 14:13:28 -0400

    Create RandomDemo.java

---
+++ CH03/RandomDemo.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class RandomDemo {
+    public static void main (String[] args) {
+        
+        // Instantiate (create) a Random object called 'fluffy'. Normally in production I wouldn't
+        // name a variable 'fluffy', but it works well for this demonstration.
+        Random fluffy = new Random();
+      
+        // Output three different random numbers
+        System.out.println(fluffy.nextInt(100));
+        System.out.println(fluffy.nextInt(100));
+        System.out.println(fluffy.nextInt(100));
+        
+        System.out.println("GAME OVER MAN!!!!");
+        
+    }
+}
