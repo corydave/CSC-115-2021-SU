634d79524fdcca3efafd9dad20e040a71c1efbe0
corydave <corydave@users.noreply.github.com>
2021-06-25 10:40:53 -0400

    Create Methods.java

---
+++ CH04/Methods.java
@@ -0,0 +1,28 @@
+// API, importing, instantiation, Methods
+
+public class Methods {
+    
+    public static void main(String[] args) {
+        
+        int num = 33;
+        System.out.println(num);
+        
+        method1();
+        
+        System.out.println(num);
+
+        System.out.println("\nGAME OVER MAN!!!");
+        
+    }
+    
+    public static void method1() {
+        
+        int num = 44;
+        System.out.println(num);
+        System.out.println("Hi");
+        
+    }
+    
+    
+    
+}
