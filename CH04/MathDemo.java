a4be90751b4651c19d2d63b9de80972d447c679a
corydave <corydave@users.noreply.github.com>
2021-06-25 10:39:49 -0400

    Create MathDemo.java

---
+++ CH04/MathDemo.java
@@ -0,0 +1,36 @@
+//  Math.max, pow, random, round
+// https://docs.oracle.com/javase/8/docs/api/index.html?java/lang/Math.html
+
+public class MathDemo {
+    
+    public static void main (String[] args) {
+    
+        mathDemo();
+    
+        System.out.println("\nGAME OVER MAN!!!");    
+        
+    }
+    
+    public static void mathDemo() {
+        
+        // System.out.println(Math.pow(16, 3));
+        // System.out.println("RANDOM NUMBER = " + Math.random());
+        // System.out.println((int)(Math.random() * 10));
+        
+        // System.out.println(Math.max(3, 9));
+        
+        int a = 10;
+        int b = 12;
+        int c = 14;
+        
+        // System.out.println(Math.max(Math.max(a, b), c));
+        // System.out.println(Math.min(a, b));
+        
+        System.out.println(Math.round(4.5));
+        
+        
+        
+        
+    }
+
+}
